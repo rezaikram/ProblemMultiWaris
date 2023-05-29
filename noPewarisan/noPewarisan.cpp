8d9b33d6729c04eca2078a455a856cc923b82edf
rezaikram <muhammadikramriza@gmail.com>
2023-05-29 21:40:22 +0700

    Add project files.

---
+++ noPewarisan/noPewarisan.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class baseClass {
+public:
+	virtual void perkenalan() {
+		cout << "Hallo saya Function dari base class";
+	}
+};
+class derivedClass :public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo says function dari derived Class";
+	}
+};
+int main() {
+	derivedClass a;
+	a.perkenalan();
+
+	return 0;
+}
